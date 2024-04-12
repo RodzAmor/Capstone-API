3e188381cdda3b42264e13862a3864d7293daf08
Rodz Andrie Amor <ramorstem2022@example.com>
2024-04-12 14:34:29 -0400

    Several improvements with the backend in terms of optimization. However, decided to stop using Azure due to problems with deployment:

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn app:app
-release: python -m spacy download en_core_web_sm
--- app.py
+++ app.py
@@ -6,11 +6,14 @@ import spacy
 from sentence_transformers import SentenceTransformer, util
 import torch
 
-
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+# cors = CORS(app, resources={r"/*": {"origins": "*", "allow_headers": "*", "methods": "*"}}, supports_credentials=True)
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, supports_credentials=True)
 
-# Load models outside
+
+
+
 model_name = "all-MiniLM-L6-v2"
 model = SentenceTransformer(model_name)
 nlp = spacy.load("en_core_web_sm")
@@ -35,10 +38,8 @@ def process_file(file_path, year, headline):
     segments = segment_text(row['Risk Factors Text'])
     headline_embedding = model.encode([headline], batch_size=32, convert_to_tensor=True)
 
-    # Encode all segments at once
     segment_embeddings = model.encode(segments, batch_size=32, convert_to_tensor=True)
 
-    # Calculate similarities in a vectorized manner
     similarities = util.pytorch_cos_sim(headline_embedding, segment_embeddings)[0]
 
     highest_similarity, highest_similarity_idx = torch.max(similarities, dim=0)
@@ -55,6 +56,30 @@ def process_file(file_path, year, headline):
     }
 
 
+@app.route('/analyze-company', methods=['GET'])
+def analyze_company():
+    headline = request.args.get('headline', default=None, type=str)
+    year = request.args.get('year', default=2024, type=str)
+    ticker = request.args.get('ticker', default=2024, type=str)
+    print("Starting Analysis")
+    print(f"Headline: {headline}")
+    
+    if headline is None:
+        return jsonify({"Headline is required.": ""}), 500
+    if year is None:
+        return jsonify({"Year is required.": ""}), 500
+    if year is None:
+        return jsonify({"Ticker is required.": ""}), 500
+
+    file = f"./companies/{ticker.upper()}.csv"
+    result = [process_file(file, year, headline)]
+
+    result = pd.DataFrame(result)
+    result = result.to_json(orient='records')
+    print("Finished Analysis\n")
+    return result
+
+
 @app.route('/analyze', methods=['GET'])
 def analyze():
     headline = request.args.get('headline', default=None, type=str)
@@ -68,20 +93,47 @@ def analyze():
     files = ['XOM.csv', 'ETR.csv', 'PXD.csv']
     results = []
     for file in files:
-        result = process_file(file, year, headline)
+        result = process_file("./companies/" + file, year, headline)
         if result:
             results.append(result)
 
     results = pd.DataFrame(results)
     response = results.to_json(orient='records')
 
-    return jsonify({"data": response})
+    return response
 
+@app.route('/company', methods=['GET'])
+def company_risk_year():
+    print("Company CSV requested")
+    ticker = request.args.get('ticker', default=None, type=str)
+    year = request.args.get('year', default=None, type=int)
 
+    if ticker is None:
+        return jsonify({"Ticker is required.": ""}), 500
+    if year is None:    
+        return jsonify({"Year is required.": ""}), 500
+
+    file_name = f"./companies/{ticker.upper()}.csv"
+    try:
+        data = load_data(file_name)
+        data['Year'] = pd.to_datetime(data['Fill Date']).dt.year
+        data = data[data['Year'] == year]
+        
+        if data.empty:
+            return jsonify({})
+
+        return data.to_json(orient='records')
+
+    except FileNotFoundError:
+        print("File Not Found")
+        return jsonify({"error": f"File for ticker {ticker.upper()} not found."}), 404
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/example', methods=['GET'])
 def example():
-    result = load_data("example2.csv")
+    result = load_data("./companies/example.csv")
     result = result.to_json(orient='records')
     
     return jsonify({"data": result})
@@ -91,5 +143,4 @@ def index():
     return "API is Online!"
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
